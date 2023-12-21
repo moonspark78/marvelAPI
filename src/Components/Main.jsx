f08ac354d6a9dde26125d9447e82903d368169d5
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-21 21:33:16 +0100

    :art:  MarvelAPI   :rocket:

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import logo from "../Images/logoa.jpg"
 
 export const Main = () => {
   return (
-    <div>Main</div>
+    <div className='header'>
+        <div className='search-bar'>
+            <img src={logo} alt='logo'/>
+            <input 
+                placeholder='Choose your Marvel ...' 
+                type='search'
+                className='search'
+            />
+        </div>
+    </div>
   )
 }
