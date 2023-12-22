6d053d781eda3478cc55e60396cfec98ba4b1a13
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-12-22 18:37:27 +0100

    :art:  MarvelAPI   :rocket:

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import hulk from "../Images/hulk.jpg"
 
 export const Card = () => {
   return (
-    <div>Card</div>
+    <div className='card'>
+        <img src={hulk} alt='img-hero'/>
+        <div className='title'>
+            <h3>Hulk</h3>
+        </div>
+    </div>
   )
 }
